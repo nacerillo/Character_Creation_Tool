b65b53d364cdde80cf0b5aaa3928adbe830e0ecb
Zach Hornung <Zachhornung@comcast.net>
2021-02-02 11:48:34 -0800

    added function to get stats from table

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
-
+//start of variables for race constructor
 var races = ['Human', 'Elf', 'Dwarf', 'Orc', 'Halfling'];
 var str = [1, 0, 0, 0, 0];
 var dex = [1, 2, 0, 2, 2];
@@ -7,6 +7,7 @@ var con = [1, 0, 2, 2, 0];
 var intel = [1, 2, 0, 0, 0];
 var wis = [1, 0, 2, 0, 0];
 var cha = [1, 0, 0, 0, 2];
+//end of variables for race constructor
 var race = document.getElementById('race');
 selectedRace = (race.value).toLowerCase();
 console.log(selectedRace); 
@@ -30,15 +31,18 @@ function Race(race, str, dex, con, intel, wis, cha){
 };
 Race.allRaces = [];
 
-function Character(){
-  this.race = ''
+function Character(cName, race, gender, avatar, bio){
+  this.name = cName;
+  this.race = race;
+  this.gender = gender;
+  this.avatar = avatar;
+  this.bio = bio;
+  this.stats = [];
   Character.allCharacters.push(this);
 }
 Character.allCharacters = [];
-Character.prototype.pName = playerName; // could move this to a player constructor to denote which characters a player has made, and the number of different players. stretch goal perhaps
-Character.prototype.cName = characterName;
-Character.prototype.gender = avatar;
-Character.prototype.bio = bio;
+// Character.prototype.pName = playerName; // could move this to a player constructor to denote which characters a player has made, and the number of different players. stretch goal perhaps
+
 
 
 for (var i = 0; i < races.length; i++){
@@ -52,3 +56,15 @@ for (var i = 0; i < races.length; i++){
   }
 }
 console.log(charRace);
+var userChar = new Character(characterName, charRace, gender, avatar, bio);
+console.log(userChar);
+
+function getStats(){
+  var str = (document.getElementById('spinnerSTR').value);
+  var dex = (document.getElementById('spinnerDEX').value);
+  var con = (document.getElementById('spinnerCONST').value);
+  var intel = (document.getElementById('spinnerINT').value);
+  var wis = (document.getElementById('spinnerWIS').value);
+  var cha = (document.getElementById('spinnerCHAR').value);
+  return [str, dex, con, intel, wis, cha];
+}
