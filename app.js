885093e007097c02d88626b6eaa2ed04966ba9f3
nacerillo <nicholascerillo@gmail.com>
2021-02-02 13:44:23 -0800

    moved update logic into updateRow function

--- app.js
+++ app.js
@@ -5,74 +5,100 @@
 var dexMod = document.getElementById('dex-mod');
 var conMod = document.getElementById('const-mod');
 
-//Strength Row Elements
+/////////////Strength Row Elements////////
+
+//Input
 var strInput = document.getElementById('spinnerSTR');
+//Mod
 var strMod = document.getElementById('str-mod');
-var strCost = document.getElementsByClassName('str-cost');
+
+//Cost
+var strCost = document.getElementById('str-cost');
 var strRace = document.getElementById('str-rt');
+
+//Actual Points
 var strAp = document.getElementById('str-ap');
-var pointsSpent = 0;
+strAp.textContent = strInput.value;
+
+//Actual Mod
+var strAm = document.getElementById('str-am');
+var strAm = strMod.textContent;
+
+
 
 var RaceMod;
 var abilityMod;
+if (2 < 5){
+console.log("hello");
+}
 function handleAbilityInput(event){
+
+    //updateRow(strAp,)
+    var val = this.value;
+    console.log(this);
     event.preventDefault();
-     console.log(this.value);
+    updateRow(val,strMod,strAp,strCost)
+    
+}
+
+function updateRow(v, mod,ap,cost){
+    ap.textContent = this.value; // this.value + Racial Bonus
+     console.log(v);
     // console.log(strCost.textContent);
      //console.log(this.value == 12);
 
-    if(this.value == 8 || this.value == 9){
-        strMod.textContent = -1;
-        if(this.value == 8){
-            strCost.textContent = -2;
+    if(v == 8 || v == 9){
+        mod.textContent = -1;
+        if(v == 8){
+            cost.textContent = -2;
         }
-        if(this.value == 9){
-            strCost.textContent = -1;
+        if(v == 9){
+            cost.textContent = -1;
         }
        // pointsSpent -= 1;
     }
 
-    else if(this.value == 10 || this.value == 11){
+    else if(v== 10 || v== 11){
     
-         strMod.textContent = 0;
-         if(this.value == 10){
-             strCost.textContent = 0;
+         mod.textContent = 0;
+         if(v == 10){
+             cost.textContent = 0;
          }
-         if(this.value == 11){
-             strCost.textContent = 1;
-             console.log(strCost.textContent);
+         if(v == 11){
+             cost.textContent = 1;
+             console.log(cost.textContent);
          }
          //pointsSpent += 0;
          //update character class with updatedMod
      }
-     else if(this.value == 12 || this.value == 13){
-        strMod.textContent = 1;
-         if(this.value == 12){
-            strCost.textContent = 2;
+     else if(v == 12 || v == 13){
+        mod.textContent = 1;
+         if(v == 12){
+            cost.textContent = 2;
         }
-        if(this.value == 13){
-            strCost.textContent = 3;
+        if(v == 13){
+            cost.textContent = 3;
         }
 
          //update character class with updated Mod
      }
      else if(this.value == 14 || this.value == 15){
-        strMod.textContent = 2;
+        mod.textContent = 2;
         if(this.value == 14){
-            strCost.textContent = 5;
+            cost.textContent = 5;
         }
         if(this.value == 15){
-            strCost.textContent = 7;
+            cost.textContent = 7;
         }
         //update character class with updated Mod
      }
      else if(this.value == 16 || this.value == 17){
-         strMod.textContent = 3;
+         mod.textContent = 3;
          if(this.value == 16){
-            strCost.textContent = 10;
+            cost.textContent = 10;
         }
         if(this.value == 17){
-            strCost.textContent = 13;
+            cost.textContent = 13;
         }
          //update character class with updated Mod 
      }
@@ -82,7 +108,7 @@ function handleAbilityInput(event){
         //update character class with updated Mod
      }
     // strMod.textContent = this.value;
-     console.log(strCost);
+     console.log(cost);
     // strMod.value = this.value;
 }
 
