870e69651bd4dabc477b175bb54f2f1f6946bff8
Zach Hornung <Zachhornung@comcast.net>
2021-02-02 16:09:42 -0800

    added functions to update table info based on character race choice

--- app.js
+++ app.js
@@ -17,19 +17,20 @@ var characterName = (document.getElementById('charactername').value);
 var gender = (document.getElementById('gender').value);
 var avatar = (document.getElementById('avatar').value);
 var bio = (document.getElementById('bio').value);
-var humanTableElement = document.getElementById('human');
-var elfTableElement = document.getElementById('elf');
-var dwarfTableElement = document.getElementById('dwarf');
-var orcTableElement = document.getElementById('orc');
-var halflingTableElement = document.getElementById('halfling');
-var tableElementArray = [humanTableElement, elfTableElement, dwarfTableElement, orcTableElement, halflingTableElement];
+var raceStrElement = document.getElementById('str-rt');
+var raceDexElement = document.getElementById('dex-rt');
+var raceConElement = document.getElementById('const-rt');
+var raceIntelElement = document.getElementById('int-rt');
+var raceWisElement = document.getElementById('wis-rt');
+var raceChaElement = document.getElementById('char-rt');
+
 var submitButton = document.getElementById('submit');
+
 
 function Player(playerName){
   this.playerName = playerName;
   this.characters = [];
 }
-
 
 function Race(race, str, dex, con, intel, wis, cha){
   this.race = race;
@@ -50,29 +51,42 @@ function Character(cName, race, gender, avatar, bio, stats){
   this.avatar = avatar;
   this.bio = bio;
   this.stats = stats;
-  // this.strength = stats[0];
-  // this.dexterity = stats[1];
-  // this.constitution = stats[2];
-  // this.intelligence = stats[3];
-  // this.wisdom = stats[4];
-  // this.charisma = stats[5];
   Character.allCharacters.push(this);
 }
 Character.allCharacters = [];
-// Character.prototype.pName = playerName; // could move this to a player constructor to denote which characters a player has made, and the number of different players. stretch goal perhaps
-
-
 
 for (var i = 0; i < races.length; i++){
   new Race(races[i], str[i], dex[i], con[i], intel[i], wis[i], cha[i]);
 };
 console.log(Race.allRaces);
 
+//below is to initialize a value on the table when the page is loaded
 for (var i = 0; i < races.length; i ++){
-  if (selectedRace == tableElementArray[i].id){
-    tableElementArray[i].className = 'active';
+  if (selectedRace == races[i].toLowerCase()){
+    raceStrElement.textContent = str[i];
+    raceDexElement.textContent = dex[i];
+    raceConElement.textContent = con[i];
+    raceIntelElement.textContent = intel[i];
+    raceWisElement.textContent = wis[i];
+    raceChaElement.textContent = cha[i];
   }
 }
+function forRaceListener(event){
+  event.preventDefault();
+  selectedRace = event.target.value;
+  console.log(selectedRace);
+  for (var i = 0; i < races.length; i ++){
+    if (selectedRace == races[i].toLowerCase()){
+      raceStrElement.textContent = str[i];
+      raceDexElement.textContent = dex[i];
+      raceConElement.textContent = con[i];
+      raceIntelElement.textContent = intel[i];
+      raceWisElement.textContent = wis[i];
+      raceChaElement.textContent = cha[i];
+    }
+  }
+}
+race.addEventListener('change', forRaceListener);
 
 function getStats(){
   var str = (document.getElementById('spinnerSTR').value);
