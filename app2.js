5cd290d311a3db3bb71ac93f3c697e5239ae8657
Zach Hornung <Zachhornung@comcast.net>
2021-02-03 13:01:03 -0800

    added local storage and updates character page based on user input in main page

--- app2.js
+++ app2.js
@@ -5,6 +5,11 @@ var conSheet = document.getElementById('con-sheet');
 var intSheet = document.getElementById('int-sheet');
 var wisSheet = document.getElementById('wis-sheet');
 var chaSheet = document.getElementById('cha-sheet');
+var characterNameSheet = document.getElementById('character-name-sheet');
+var playerNameSheet = document.getElementById('player-name-sheet');
+var genderSheet = document.getElementById('gender-sheet');
+var bioSheet = document.getElementById('bio-sheet');
+
 
 
 function getPlayerInfo(){
@@ -23,6 +28,10 @@ function updateFields(){
   intSheet.textContent = playerInfo.characters[0].stats[3];
   wisSheet.textContent = playerInfo.characters[0].stats[4];
   chaSheet.textContent = playerInfo.characters[0].stats[5];
+  characterNameSheet.textContent = playerInfo.characters[0].name;
+  playerNameSheet.textContent = playerInfo.playerName;
+  genderSheet.textContent = playerInfo.characters[0].gender;
+  bioSheet.textContent = playerInfo.characters[0].bio;
   return playerInfo;
 }
 updateFields();
